791c6f3e6fc4ca3985ee3c5278811120d05417dd
konnorcollins <konnor.s.collins@gmail.com>
2023-05-31 14:50:05 -0600

    Added two palettes.  Hot-swapping is WIP.

--- kanadex-client/src/App.tsx
+++ kanadex-client/src/App.tsx
@@ -11,12 +11,12 @@ function App() {
   const image = new Image(195, 164);
   image.src = testImage;
 
-  // const BASE_COLORS = ["000000", "FFFFFF", "FF40FF", "C000C0"]
+  const BASE_COLORS = ["000000", "ffffff", "ff40ff", "c000c0"]
   const altColors = new Map<string, string>()
-  altColors.set("000000", "000000");
-  altColors.set("ffffff", "ffffff");
-  altColors.set("ff40ff", "ff40ff");
-  altColors.set("c000c0", "c000c0");
+  altColors.set(BASE_COLORS[0], "182005");
+  altColors.set(BASE_COLORS[1], "769518");
+  altColors.set(BASE_COLORS[2], "3c4d0c");
+  altColors.set(BASE_COLORS[3], "182005"); 
 
 
   const [bits, setBits] = useState(2);
@@ -28,12 +28,47 @@ function App() {
     if (bits == 2) setBits(1)
   }
 
+  const handlePalleteSwap = (palette: string) =>
+  {
+    switch (palette)
+    {
+      case("bw"):
+        altColors.set(BASE_COLORS[0], "000000");
+        altColors.set(BASE_COLORS[1], "ffffff");
+        altColors.set(BASE_COLORS[2], "ff40ff");
+        altColors.set(BASE_COLORS[3], "c000c0");
+        break;
+      case("retro (dark)"):
+        altColors.set(BASE_COLORS[0], "182005");
+        altColors.set(BASE_COLORS[1], "769518");
+        altColors.set(BASE_COLORS[2], "3c4d0c");
+        altColors.set(BASE_COLORS[3], "182005");
+        break;
+      case("gothic"):
+        altColors.set(BASE_COLORS[0], "3f352d");
+        altColors.set(BASE_COLORS[1], "b3ab98");
+        altColors.set(BASE_COLORS[2], "a83d33");
+        altColors.set(BASE_COLORS[3], "7b8779");     
+        break;  
+      default:
+        altColors.set(BASE_COLORS[0], "000000");
+        altColors.set(BASE_COLORS[1], "ffffff");
+        altColors.set(BASE_COLORS[2], "ff40ff");
+        altColors.set(BASE_COLORS[3], "c000c0");
+        break;
+    }
+  }
+
   return (
     <>
       <h1>WoH BitCanvas</h1>
       <div className="card">
           <button onClick={() => handleBitToggle()}>{bits} bits (toggle not yet working)</button>
-        </div>
+      </div>
+      <div className="card">
+          <button onClick={() => handlePalleteSwap('retro (dark)')}>gameboy</button>
+          <button onClick={() => handlePalleteSwap('gothic')}>gothic</button>
+      </div>
       <div className="card">
           <BitCanvas baseImage={image} altColors={altColors} />
       </div>
@@ -41,7 +76,7 @@ function App() {
         <p>todo</p>
         <ul>
           <li>fully implement bit toggle</li>
-          <li>color palette swap</li>
+          <li>color palette swap (hotswap is broken, cold swap works)</li>
         </ul>
       </div>
       <p className="read-the-docs">
