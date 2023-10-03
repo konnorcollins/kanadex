a789f033cf310d38ce05330e7fc63e554a5c08ec
konnorcollins <konnor.s.collins@gmail.com>
2023-10-03 00:46:08 -0600

    Added sftp library

--- kanadex-api/src/attachments/storage/sftp.ts
+++ kanadex-api/src/attachments/storage/sftp.ts
@@ -1,3 +1,5 @@
+import Client from 'ssh2-sftp-client'
+
 import { Storage } from "./storage";
 
 export class SftpStorage implements Storage {
@@ -5,6 +7,7 @@ export class SftpStorage implements Storage {
     private readonly port: number;
     private readonly username: string;
     private readonly private_key: Buffer;
+    private readonly sftp_client: Client = new Client();
 
     constructor(host: string, port: number, username: string, private_key: Buffer) {
         this.host = host;
@@ -14,8 +17,19 @@ export class SftpStorage implements Storage {
     }
 
     upload(attachment: any): Promise<string> {
-        throw new Error("Method not implemented.");
+        const config: Client.ConnectOptions = {
+            host: this.host,
+            port: this.port,
+            username: this.username,
+            privateKey: this.private_key
+        };
+
+        return this.sftp_client.connect(config)
+            .then(() => {
+                return this.sftp_client.cwd();
+            })
     }
+
     download(attachment_id: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
